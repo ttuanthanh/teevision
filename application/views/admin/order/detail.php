c505ac0aa2aa4767a943df00bc0738108cff3e17
MyTrang <cntt.mytrang@gmail.com>
2016-06-13 22:11:13 +0700

    Chinh sua Detail

--- application/views/admin/order/detail.php
+++ application/views/admin/order/detail.php
@@ -97,10 +97,10 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
                     </div>
                     <br clear="all">
                     <div  class="col-md-12 button-preview">
-                        <button type="button" class="btn btn-success btn-lg btn-block">Preview Artwork</button>
+                        <button type="button" class="btn btn-info active btn-block">Preview Artwork</button>
                     </div>
                     <div class="col-md-12 button-preview">
-                        <button type="button" class="btn btn-success btn-lg btn-block">Download Artwork</button>
+                        <button type="button" class="btn btn-info active btn-block">Download Artwork</button>
                     </div>
                 </div>
                 <div class="col-md-7">
@@ -142,20 +142,24 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
                             </tbody>
                     </table>
                     </div>
-                    <div>
-                        <div class="col-md-3 button-preview">
-                            <button type="button" class="btn btn-default">Default button</button>
-                        </div>
-                        <div class="col-md-9 button-preview">
-                            <button type="button" class="btn btn-default">Default button</button>
-                        </div>
-                        <div class="col-md-3 button-preview">
-                            <button type="button" class="btn btn-default">Default button</button>
-                        </div>
-                        <div class="col-md-9 button-preview">
-                            <button type="button" class="btn btn-default">Default button</button>
-                        </div>
-                    </div>
+                    <div>       
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                            <div class="col-md-6 button-preview">
+                                <button type="button" class="btn btn-info active btn-block">Default button 1</button>
+                            </div>
+                            <div class="col-md-6 button-preview">
+                                <button type="button" class="btn btn-info active btn-block">Default button 2</button>
+                            </div>
+                            <div class="col-md-6 button-preview">
+                                <button type="button" class="btn btn-info active btn-block">Default button 3</button>
+                            </div>
+                            <div class="col-md-6 button-preview">
+                                <button type="button" class="btn btn-info active btn-block">Default button 4</button>
+                            </div>
+                        </div>  
+                        <div class="col-md-2"></div>
+                    </div>                    
                 </div>
             </div>	
         </div>
@@ -168,11 +172,111 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	</div>
 	<div class="panel-body" id="panelbody">
             <div class="row">
-                
+                <table class="table table-reflow">
+                    <tbody>
+                        <?php if ($address !== false) { ?>										
+                            <?php foreach ($address as $key => $value) { ?>
+                            <tr class="row">
+                                    <th><?php echo $key; ?>:</th>
+                                    <td><?php echo $value; ?></td>
+                            </tr>
+                            <?php } ?>
+                        <?php } ?>
+                    </tbody>
+                  </table>
             </div>
         </div>
     </div>
-    <div class="col-md-6 no-padding-right">
+    <div class="col-md-6 no-padding-right">  
+        <div class="row">
+            <div class="col-md-12">
+                    <table id="table_order_detail" class="table table-bordered table-hover">
+                            <thead>
+                                    <tr>
+                                            <th class="center" style="width: 7%;"><?php echo lang('orders_admin_view_design_title'); ?></th>
+                                            <th class="center" style="width: 18%;"><?php echo lang('name'); ?></th>					
+                                            <th class="center" style="width: 7%;"><?php echo lang('orders_admin_product_price_title'); ?></th>
+                                            <th class="center" style="width: 7%;"><?php echo lang('orders_admin_product_qty_title'); ?></th>
+                                            <th class="center"  style="width: 10%;"><?php echo lang('total'); ?></th>
+                                    </tr>
+                            </thead>
+                            <tbody>					
+                                    <?php 
+                                            $total = 0;
+                                            $count = 1;
+                                            $shipping_price = $order->shipping_price;
+                                            $payment_price = 0.0;
+                                    ?>
+                                    <?php foreach($items as $product){?>
+                                            <tr class="top-align">
+                                                    <td class="center"><a class="fancybox fancybox.iframe" href="<?php echo site_url().'admin/orders/view/'.$product->id;?>" ><?php echo lang('view');?></a></td>
+                                                    <td>
+                                                            <a href="<?php echo site_url('admin/products/edit/'.$product->product_id); ?>" title="<?php echo $product->product_name; ?>">
+                                                                    <strong><?php echo $product->product_name; ?></strong>
+                                                            </a>
+                                                    </td>
+                                                    <td class="right"><?php echo $setting->currency_symbol.number_format($product->product_price, 2);?></td>
+                                                    <td class="right"><?php echo $product->quantity;?></td>
+                                                    <?php $total_row = $product->quantity*($product->product_price+$product->price_print+$product->price_clipart)+$product->price_attributes;?>
+                                                    <td class="right"><?php echo $setting->currency_symbol.number_format($total_row, 2);?></td>
+                                            </tr>
+                                            <?php 
+                                                    $total = $total+$total_row;
+                                                    $count++;
+                                            ?>
+                                    <?php } ?>
+                                    <!-- shipping -->
+                                    <tr>
+                                            <td colspan="4" class="right">
+                                                    <?php echo lang('orders_admin_shipment_fee_title');?>
+
+                                                    <?php if (count($shipping)) { ?>								
+                                                            <br><small><?php echo lang('orders_admin_shipping_method'); ?>: <a href="<?php echo site_url('admin/settings/shipping'); ?>"><strong><?php echo $shipping->title; ?></strong></a></small>
+                                                            <br><small><?php echo $shipping->description; ?></small>
+                                                    <?php } ?>
+
+                                            </td>
+                                            <td class="right"><?php echo $setting->currency_symbol.number_format($shipping_price, 2);?></td>
+                                    </tr>
+
+                                    <!-- payment -->
+                                    <tr>
+                                            <td colspan="4" class="right">
+                                                    <?php echo lang('orders_admin_payment_fee_title');?>
+
+                                                    <?php if (count($payment)) { ?>								
+                                                            <br><small><?php echo lang('orders_admin_payment_method'); ?>: <a href="<?php echo site_url('admin/settings/payment'); ?>"><strong><?php echo $payment->title; ?></strong></a></small>
+                                                            <br><small><?php echo $payment->description; ?></small>
+                                                    <?php } ?>
+                                            </td>
+                                            <td class="right"><?php echo $setting->currency_symbol.number_format($payment_price, 2) ;?></td>
+                                    </tr>
+
+                                    <!-- discount -->
+                                    <tr>
+                                            <td colspan="4" class="right">
+                                                    <?php echo lang('orders_admin_discount');?>
+
+                                                    <?php if (count($discount)) { ?>								
+                                                            <br><small><?php echo $discount->name; ?>: <a href="<?php echo site_url('admin/coupon/edit/'.$discount->id); ?>"><strong><?php echo $discount->code; ?></strong></a></small>								
+                                                    <?php } ?>
+                                            </td>
+                                            <td class="right"><?php echo $setting->currency_symbol.number_format($order->discount, 2) ;?></td>
+                                    </tr>
+
+                                    <!-- total -->
+                                    <tr>
+                                            <?php $total = $total + $shipping_price - $order->discount; ?>
+                                            <td colspan="4" class="right"><?php echo lang('orders_admin_total_title');?></td>
+                                            <td class="right" colspan="4"><strong><?php echo $setting->currency_symbol.number_format($total, 2);?><strong></td>
+                                    </tr>
+                            </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+    <br clear="all">   
+    <div class="col-md-6 no-padding">
         <div class="panel panel-default">
             <div class="panel-heading">
                     <i class="fa fa-external-link-square icon-external-link-sign"></i>
@@ -180,23 +284,26 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
             </div>
             <div class="panel-body" id="panelbody">
                 <div class="row">
-
+                    <table class="table table-reflow">
+                        <tbody>
+                          <tr>     
+                            <th>Client Name:</th>
+                            <td>Tran Thi My Trang</td>
+                          </tr>
+                          <tr>
+                            <th>Shipping Adress:</th>
+                            <td>793/35/15 Tran Xuan Soan, P.Tan quy q.7</td>
+                          </tr>
+                          <tr>
+                           <th>Telephone:</th>
+                            <td>263261973027</td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
-    </div>
-    <br clear="all">
-   <div class="panel panel-default col-md-6 no-padding">
-	<div class="panel-heading">
-		<i class="fa fa-external-link-square icon-external-link-sign"></i>
-		Information
-	</div>
-	<div class="panel-body" id="panelbody">
-            <div class="row">
-                
-            </div>
-        </div>
-    </div>
+    </div>    
     <div class="col-md-6 no-padding-right">
         <div class="panel panel-default">
             <div class="panel-heading">
