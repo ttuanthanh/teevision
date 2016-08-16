4897b8bdcbe897977868c5060b3f4e91a4395a53
TuanThanh <tuanthanh.tran@nhnent.com>
2016-08-16 15:40:49 +0700

    new SQL

---
+++ sql/aws_tshirt.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : tshirt-aws
+Source Server Version : 50546
+Source Host           : 54.236.222.177:3306
+Source Database       : tshirt
+
+Target Server Type    : MYSQL
+Target Server Version : 50546
+File Encoding         : 65001
+
+Date: 2016-08-16 15:39:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
