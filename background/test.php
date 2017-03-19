a2ccde28450ea7895223832399a75d5398c06fde
MyTrang <cntt.mytrang@gmail.com>
2017-03-19 21:54:34 +0700

    Edit layout

--- background/test.php
+++ background/test.php
@@ -14,37 +14,37 @@ if(isset($_POST["submit"])) {
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "<br />File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "<br />File is not an image.";
         $uploadOk = 0;
     }
 
     // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
+//    if (file_exists($target_file)) {
+//        echo "<br />Sorry, file already exists.";
+//        $uploadOk = 0;
+//    }
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        echo "<br />Sorry, your file is too large.";
         $uploadOk = 0;
     }
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "<br />Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "<br />Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            echo "<br />The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 
             $strInputFile = $target_file;
             $tranname= explode('.', $strInputFile);
@@ -58,7 +58,7 @@ if(isset($_POST["submit"])) {
             $im->writeImage($target);
             $im->destroy();
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "<br />Sorry, there was an error uploading your file.";
         }
     }
 }
@@ -78,27 +78,27 @@ if(isset($_POST["submit"])) {
 <form  method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Upload and remove background" name="submit">
 </form>
-
-<div style="background: #007F7B">
-    <div style="float: left; width: 50%">
-        <h2>INPUT: <?php echo $strInputFile; ?></h2>
-        <img src="<?php echo $strInputFile?>" width="100%">
-    </div>
 <?php 
 if (isset($target)){
     ?>
+<div style="background: #007F7B">
+    <div style="float: left; width: 50%">
+        <h2>INPUT: <?php echo $strInputFile; ?></h2>
+        <img src="<?php echo $strInputFile?>" width="80%">
+    </div>
+
     <div style="float: left; width: 49%">
         <h2>OUTPUT: <?php echo $target; ?></h2>
-        <img src="<?php echo $target?>"  width="100%">
+        <img src="<?php echo $target?>"  width="80%">
     </div>
-    <?php
-    }
-    ?>
+    
     
     <br clear="all" />
 </div>
 
-
+<?php
+    }
+    ?>
 
