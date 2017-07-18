436df7dde880ce02814bb68de1b7c23c1f280a42
MyTrang <cntt.mytrang@gmail.com>
2017-07-18 21:51:08 +0700

    fix bug save design

--- assets/js/design.js
+++ assets/js/design.js
@@ -3987,6 +3987,7 @@ var design = {
         var image = design.output.front.toDataURL();
         var teams = JSON.stringify(design.teams);
         var productColor = design.exports.productColor();
+        var productColorTitle = design.exports.productColorTitle();
         var attributes = jQuery('#tool_cart').serialize();
         if (attributes != '') {
             var obj = JSON.parse('{"' + decodeURI(attributes).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
@@ -4017,6 +4018,7 @@ var design = {
             'design_name': design.design_name,
             'design_note': jQuery('#design_note').val(),
             'product_color': productColor,
+            'product_color_title':productColorTitle,
             'price': "",
             'sizehtml': phrase
         };
