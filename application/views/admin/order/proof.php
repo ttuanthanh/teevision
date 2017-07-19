0b0c99632b399abfddd3b116d412ee5ec822fbf1
MyTrang <cntt.mytrang@gmail.com>
2017-07-19 23:49:08 +0700

    Project 9 Task: 17

--- application/views/admin/order/proof.php
+++ application/views/admin/order/proof.php
@@ -15,6 +15,12 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 <link href="<?php echo base_url('assets/plugins/jasny-bootstrap/css/jasny-bootstrap.min.css'); ?>" rel="stylesheet"/>
 <script src="<?php echo base_url('assets/plugins/jasny-bootstrap/js/jasny-bootstrap.min.js');?>"></script>
 <script src="<?php echo base_url('assets/plugins/holder/holder.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jssor.slider-23.0.0.mini.js');?>"></script>
+<link href="<?php echo base_url('assets/plugins/bxSlider/jquery.bxslider.css');?>" rel="stylesheet" />
+<script src="<?php echo base_url('assets/plugins/bxSlider/jquery.bxslider.js');?>"></script>
+<!--<link rel="stylesheet" type="text/css" href="/assets/plugins/CustomFileInputs/css/normalize.css" />-->
+<!--<link rel="stylesheet" type="text/css" href="/assets/plugins/CustomFileInputs/css/demo.css" />-->
+<link rel="stylesheet" type="text/css" href="/assets/plugins/CustomFileInputs/css/component.css" />
 <style>
     .info-table{
         margin-left: 2px;
@@ -198,12 +204,12 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
                             <p style="margin: 10px 20px 0; font-weight: bold">Add Proof: </p>
                             <div class="col-md-6 col-md-offset-1 r-border">
                                 <?php
-                                $attribute = array('class' => 'form-horizontal', 'id' => 'form-proof-'.$product->id);		
+                                $attribute = array('class' => 'form-horizontal', 'id' => 'form-proof-'.$product->id, 'enctype'=>'multipart/form-data');		
                                 echo form_open(site_url('admin/proof/save'), $attribute);
                                 
                                 //var_dump($proofs);
                                 ?>
-                                <div class="fileinput <?php echo (isset($proof->proof_file) && $proof->proof_file != '') ? 'fileinput-exists' : 'fileinput-new' ?> " data-provides="fileinput">
+<!--                                <div class="fileinput <?php echo (isset($proof->proof_file) && $proof->proof_file != '') ? 'fileinput-exists' : 'fileinput-new' ?> " data-provides="fileinput">
                                     <table>
                                         <tr>
                                             <td>
@@ -228,9 +234,59 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
                                     <input type="hidden" id="item_id" name="item_id" value="<?php echo $product->id; ?>">
                                     <input type="hidden" id="proof_id" name="proof_id" value="<?php echo isset($proof->id) ? $proof->id : ''; ?>">
 
+                                </div>-->
+                                <div class="slider col-md-9">
+                                    <ul class="bxslider">
+                                        <?php
+                                        if(!isset($product->artworkImage) || $product->artworkImage == "")
+                                            echo '<img src="/assets/images/photo.png" />';
+                                        else
+                                            foreach($product->artworkImage as $image){ ?>
+                                                <li>
+                                                    <img src="<?php echo $image->url; ?>" />
+                                                    <div style="text-align: right;">
+                                                        <a target="_blank" href="<?php echo $image->url; ?>" class="fa fa-download" style="margin-right: 30px;padding-left: 10px "> download</a>
+                                                        <a class="fa fa-remove" href="/admin/proof/deleteImage/<?php echo $image->id; ?>" style="margin-right: 30px;padding-left: 10px "> remove</a>
+                                                    </div>
+
+                                                </li>
+                                            <?php
+                                            }
+                                        ?>
+
+                                    </ul>
+                                </div>
+                                <div class="row-content clearfix">                                
+                                    <br/><br/>
+                                    <input type="file" name="file[]" id="file_<?php echo $product->id ?>" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple />
+                                    <label for="file_<?php echo $product->id ?>"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> 
+                                        <span>Select Upload file &hellip;</span>
+                                    </label>
+                                    <div style="font-size: 10px; font-style: italic; font-weight: bold">*Can upload multi image</div>
+                                    <hr/>
+                                    <br />
+                                    <p><b>Order description</b></p>
+                                    <textarea name="order_des" rows="3" style="width:100%; margin-bottom: 10px" placeholder="Enter order description"><?php 
+                                        if(isset($proof->order_des)) echo $proof->order_des;
+                                    ?></textarea>
+                                    <br />
+                                    <div class="row-content clearfix text-right" style="margin: 30 auto;">
+                                        <br/><br/>
+                                        <button type="submit" class="btn btn-primary" style="width: 70%;">Save</button>
+                                    </div>                            
+                                    <input type="hidden" id="design-image-front<?php echo $product->id ?>" name="front_file" value="<?php echo isset($artwork->front_file) ? $artwork->front_file : '' ?>">
+                                    <input type="hidden" id="design-image-back<?php echo $product->id ?>" name="back_file" value="<?php echo isset($artwork->back_file) ? $artwork->back_file : '' ?>">
+                                    <input type="hidden" id="order_id" name="order_id" value="<?php echo $order->id; ?>">
+                                    <input type="hidden" id="item_id" name="item_id" value="<?php echo $product->id; ?>">
+                                    <input type="hidden" id="proof_id" name="proof_id" value="<?php echo isset($proof->id) ? $proof->id : ''; ?>">
+                                    <br clear="all"/>
                                 </div>
                                 
                                 <hr>
+                                
+                                <?php echo form_close(); ?>
+                            </div>
+                            <div class="col-md-5 text-center">
                                 <div class="bold">
                                     <?php 
                                         if(isset($proof->id)){
@@ -241,18 +297,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
                                         <h4 class="text-center">Time: <?php echo $newDate->format('H:i'); ?></h4>
                                         <?php } ?>
                                 </div>
-                                <p><b>Order description</b></p>
-                                <textarea name="order_des" rows="3" style="width:100%; margin-bottom: 10px" placeholder="Enter order description"><?php 
-                                    if(isset($proof->order_des)) echo $proof->order_des;
-                                ?></textarea>
-                                <br />
-                                <div class="clearfix text-right" style="margin-right: 20px">                        
-                                    <button type="submit" class="btn btn-primary">Save</button>
-                                </div>
-                                <?php echo form_close(); ?>
-                            </div>
-                            <div class="col-md-5 text-center">
-                                
+                                <hr />
                                 <h4 class="text-left">Ready to print?</h4>
                                 <?php    
                                 if(isset($proof->id)){ 
@@ -313,7 +358,10 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
         //if(typeof Holder !== 'undefined') Holder.run();
         var baseURL	= '<?php echo base_url(); ?>';
         
+    $(document).ready(function(){
+        $('.bxslider').bxSlider();
         
+    }); 
         
         
         
@@ -321,3 +369,4 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 </script>
 <script type="text/javascript" src="<?php echo site_url().'assets/admin/js/artwork.js'; ?>"></script>
 <script type="text/javascript" src="<?php echo site_url().'assets/admin/js/comment.js'; ?>"></script>
+<script src="/assets/plugins/CustomFileInputs/js/custom-file-input.js"></script>
