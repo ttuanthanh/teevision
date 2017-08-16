9e0ee1dcedd4fac39a130a6b276b998932c48295
HoangHai <hoanghai.le>
2017-08-16 08:53:19 +0700

    change tree clippart

--- assets/js/dg-function.js
+++ assets/js/dg-function.js
@@ -145,6 +145,79 @@ var dgUI = {
 				}
 			});
 
+		}, modalTree:function(e){
+			jQuery(e).dynatree({
+				onActivate: function (node) {
+					jQuery('.dynatree-active a').attr('data-id', node.data.id);
+				},
+				onLazyRead: function(node){
+					// Mockup a slow reqeuest ...
+					node.appendAjax({
+						url: "sample-data2.json",
+						debugLazyDelay: 750 // don't do this in production code
+					});
+				},
+
+				dnd: {
+					onDragStart: function(node) {
+						/** This function MUST be defined to enable dragging for the tree.
+						 *  Return false to cancel dragging of node.
+						 */
+						logMsg("tree.onDragStart(%o)", node);
+						return true;
+					},
+					onDragStop: function(node) {
+						// This function is optional.
+						logMsg("tree.onDragStop(%o)", node);
+					},
+					autoExpandMS: 1000,
+					preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
+					onDragEnter: function(node, sourceNode) {
+						/** sourceNode may be null for non-dynatree droppables.
+						 *  Return false to disallow dropping on node. In this case
+						 *  onDragOver and onDragLeave are not called.
+						 *  Return 'over', 'before, or 'after' to force a hitMode.
+						 *  Return ['before', 'after'] to restrict available hitModes.
+						 *  Any other return value will calc the hitMode from the cursor position.
+						 */
+						logMsg("tree.onDragEnter(%o, %o)", node, sourceNode);
+						return true;
+					},
+					onDragOver: function(node, sourceNode, hitMode) {
+						/** Return false to disallow dropping this node.
+						 *
+						 */
+						logMsg("tree.onDragOver(%o, %o, %o)", node, sourceNode, hitMode);
+						// Prevent dropping a parent below it's own child
+						if(node.isDescendantOf(sourceNode)){
+							return false;
+						}
+						// Prohibit creating childs in non-folders (only sorting allowed)
+						if( !node.data.isFolder && hitMode === "over" ){
+							return "after";
+						}
+					},
+					onDrop: function(node, sourceNode, hitMode, ui, draggable) {
+						/** This function MUST be defined to enable dropping of items on
+						 * the tree.
+						 */
+						logMsg("tree.onDrop(%o, %o, %s)", node, sourceNode, hitMode);
+						sourceNode.move(node, hitMode);
+
+						jQuery.post("categories/sorting", { 'parent': node.data.id, 'child': sourceNode.data.id, 'action': hitMode},
+							function(data) {});
+						// expand the drop target
+						// sourceNode.expand(true);
+					},
+					onDragLeave: function(node, sourceNode) {
+						/** Always called if onDragEnter was called.
+						 */
+						logMsg("tree.onDragLeave(%o, %o)", node, sourceNode);
+					}
+				}
+			});
+
+
 		}
 	},
 	art:{
