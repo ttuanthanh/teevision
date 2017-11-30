d5673cabb8c87ed8c2921d456a65e34a220e55c5
HoangHai <hoanghai.le>
2017-11-30 12:31:31 +0700

    fix loading

--- assets/js/homep.js
+++ assets/js/homep.js
@@ -1,3 +1,6 @@
+$(function() {
 $('.load-more').on("click", function(){
-    $(".item-group").append('<div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="./t-shirt images/shirt1.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="./t-shirt images/shirt2.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="./t-shirt images/shirt3.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="./t-shirt images/shirt4.png"> </div> </div>');
+    $(".item-group").append('<div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="/assets/images/newIU/shirt1.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="/assets/images/newIU/shirt2.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="/assets/images/newIU/shirt3.png"> </div> </div> <div class="item col-md-3 col-lg-3"> <div class="item-padding"> <img align="middle" src="/assets/images/newIU/shirt4.png"> </div> </div>');
+});
+
 });
